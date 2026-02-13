a67230a5edbf750df5ff930154a5e0d8ac2f0db9
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-02-13 16:40:27 +0530

    13/02.2026

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,35 +1,14 @@
-import './App.css';
-import EventToDo from './components/EventToDo';
+import './App.css'
+import EventToDo from './EventToDo'
+import Counter from './Counter'
 
 function App() {
   return (
     <>
-      <EventToDo name="Vishal kumar" />
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        alert('Form submitted');
-      }}>
-        <input type="text" placeholder="Enter something" />
-        <button type="submit">Submit</button>
-
-      </form>
-      <ul id ="taskList">
-        <li>
-            <input type="checkbox" id="task1" name="task1" value="Task 1" />
-            <label for="task1"> Task 1</label>
-        </li>
-        <li>
-            <input type="checkbox" id="task2" name="task2" value="Task 2" />
-            <label for="task2"> Task 2</label>
-
-        </li>
- 
-
-          
-      </ul>
-
+      {/* <EventToDo /> */}
+      <Counter />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- react-project/src/components/EventToDo.jsx
+++ react-project/src/components/EventToDo.jsx
@@ -1,10 +1,42 @@
-export default function EventToDo({ name }) {
+import { useState } from "react";
+
+export default function EventToDo() {
+  const [tasks, setTasks] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const inputValue = e.target.task.value.trim();
+
+    if (inputValue === "") {
+      alert("Task cannot be empty");
+      return;
+    }
+
+    setTasks([...tasks, inputValue]);
+    e.target.reset();
+  }
+
+  function handleDelete(index) {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  }
+
   return (
     <div>
-      <h1>Event To Do</h1>
-      <button onClick={() => alert(`Hello ${name}`)}>
-        Click Me
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="task" placeholder="Add your task here" />
+        <button type="submit">Add Task</button>
+      </form>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span>{task}</span>
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
